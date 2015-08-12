ce881ea9e88e28b07ab7701a6ec8f1504fb8cbfd
Yves Yuen <yves.j.yuen@gmail.com>
2015-08-12 17:28:13 -0400

    spawning grass

--- index.js
+++ index.js
@@ -6,40 +6,21 @@ var dirt = ['dirt','dirt','dirt'];
 
 var materials= [grass, dirt];
 
-//Instanciate world Array 10X10 map
-var worldArray =[];
-for(var x= 0; x<=10; x++){
-  worldArray[x] = new Array(10);
-}
-
-//Instanciate all map to dirt;
-for(var x= 0; x<=10; x++){
-  for(var y=0 ; y<=10;y++){
-    worldArray[x][y] = new Cell("dirt");
-  }
-}
-
+var Map = require('./Map');
 //set 1X1 to be grass;
-worldArray[1][1].material="grass";
-
-function color(x,y,z){
-  return worldArray[x][z]["material"]==="dirt" ? 2 : 1;
-}
-
-
-
-function Cell(material) {
-  this.material = material;
-  // this.neighbor = [//cells];
-}
+var map = new Map(10);
+map.fertilize(5,5);
 
 var game = createGame({
     generate: function(x,y,z){
-      return (y === 0 && x>=0 && x<=10 && z>=0 && z<=10) ? color(x,y,z) : 0;
+      return (y === 0 && x>=0 && x<=10 && z>=0 && z<=10) ? map.getMaterial(x,z) : 0;
     },
     materials: materials,
     texturePath: './textures/'
 });
+
+
+
 
 
 //voxel-player: add player that can move around. It needs a copy of the game
@@ -54,7 +35,11 @@ player.yaw.position.set(0,10,0);
 window.player=player;
 
 window.game = game; //for debugging
+window.map = map;
 var container = document.body;
 game.appendTo(container);
 
-return game;
+
+game.setInterval(function() {
+  map.growGrass(game);
+}, 2000);
