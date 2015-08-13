1254175e20e7ee464a068e67d6c16ba3c2b8ca21
Justin Kim <jkim430@gmail.com>
2015-08-13 11:48:47 -0400

    randomized grassgrow

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-
 // <------ MATERIALS ------>
 var grass = ['grass', 'dirt', 'grass_dirt'];
-var dirt = ['dirt','dirt','dirt'];
-var materials= [grass, dirt];
-
+var dirt = ['dirt', 'dirt', 'dirt'];
+var materials = [grass, dirt];
 
 // <------ MAP ------>
 var Map = require('./Map');
 // Create Map
 var map = new Map(10);
 window.map = map;
-map.fertilize(5,5);
+map.fertilize(5, 5);
 
 // <------ GAME ------>
 //voxel-engine: base module
 var createGame = require('voxel-engine');
 var game = createGame({
-    generate: function(x,y,z){
-      return (y === 0 && x>=0 && x<=10 && z>=0 && z<=10) ? map.getMaterial(x,z) : 0;
+    generate: function(x, y, z) {
+        return (y === 0 && x >= 0 && x <= 10 && z >= 0 && z <= 10) ? map.getMaterial(x, z) : 0;
     },
     materials: materials,
-    texturePath: './textures/'
+    texturePath: './textures/',
+    controls: {
+        discreteFire: true
+    }
 });
 
 
@@ -30,7 +31,7 @@ game.appendTo(container);
 
 
 var createCreature = require('voxel-creature')(game);
-var creature = createCreature((function () {
+var creature = createCreature((function() {
     var T = game.THREE;
     var body = new T.Object3D();
 
@@ -44,7 +45,7 @@ var creature = createCreature((function () {
     head.position.set(0, 5, 0);
     body.add(head);
 
-    var eyes = [0,1].map(function () {
+    var eyes = [0, 1].map(function() {
         var eye = new T.Mesh(
             new T.CubeGeometry(1, 1, 1),
             new T.MeshLambertMaterial({
@@ -67,24 +68,38 @@ creature.position.y = 2;
 creature.position.x = 2;
 creature.position.z = 2;
 // <------ PLAYER ------>
+
 //voxel-player: add player that can move around. It needs a copy of the game
 var createPlayer = require('voxel-player')(game);
 var player = createPlayer('textures/player.png'); //creates player and provide dummy texture
-window.player=player;
+window.player = player;
 // player.pov('third');
 player.possess(); //camera follow player
-player.yaw.position.set(1,10,1);
+player.yaw.position.set(1, 10, 1);
 //Toggle Camera First / Third Person View
-window.addEventListener('keydown', function (ev) {
+window.addEventListener('keydown', function(ev) {
     if (ev.keyCode === 'R'.charCodeAt(0)) {
         player.toggle();
     }
 });
 
 
+var highlight = require('voxel-highlight')
+var highlighter = highlight(game)
+var positionME;
+highlighter.on('highlight', function(voxelPosArray) {
+    positionME = voxelPosArray
+})
+
+game.on('fire', function(pos) {
+    console.log(pos)
+    // var position = positionME;
+    // if (position) game.createBlock(positionYES, 1);
+    // else game.setBlock(position, 0)
+})
 
 
 // <------ TICK ------>
 game.setInterval(function() {
-  map.growGrass(game);
+    map.growGrass(game);
 }, 2000);
