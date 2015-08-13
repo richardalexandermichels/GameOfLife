53e2f11694cb411402b381c630386b6165b4844a
celanajaya <petermsteele@gmail.com>
2015-08-13 16:52:44 -0400

    merged

--- forest/index.js
+++ forest/index.js
@@ -1,33 +1,31 @@
 var Tree = require('voxel-forest');
 module.exports = function(game) {
-    this.treeArr = [];
-    this.createForest = function(x,y,z) {
-        var randCoords = function() {
-            var randX = Math.floor(Math.random() * map.size) + 1;
-            var randZ = Math.floor(Math.random() * map.size) + 1;
-            var randH = Math.floor(Math.random() * map.size) + 4;
-            if (treeArr.length) {
-                for (var i = 0; i < treeArr.length; i++) {
-                    if (treeArr[i][0] === randX || treeArr[i][1] === randZ) {
-                        randCoords();
-                    }
-                    return [randX, randZ, randH];
+    var treeArr = [];
+    var randCoords = function() {
+        var randX = Math.floor(Math.random() * map.size) + 1;
+        var randZ = Math.floor(Math.random() * map.size) + 1;
+        var randH = Math.floor(Math.random() * map.size) + 4;
+        if (treeArr.length) {
+            for (var i = 0; i < treeArr.length; i++) {
+                if (treeArr[i][0] === randX || treeArr[i][1] === randZ) {
+                    randCoords();
                 }
+                return [randX, randZ, randH];
             }
-            return [randX, randZ, randH];
-        };
-        var density = Math.floor(map.size / 5);
-
-        for(var i = 0; i < density; i++) {
-            var treePos = randCoords();
-            treeArr.push(treePos);
-            Tree(game, {
-                position:{x: treePos[0], y:0, z:treePos[1]},
-                height: treePos[2],
-                bark: 3,
-                leaves: 4,
-                treeType: 'subspace'
-            });
         }
+        return [randX, randZ, randH];
+    };
+    var density = Math.floor(map.size / 5);
+
+    for(var i = 0; i < density; i++) {
+        var treePos = randCoords();
+        treeArr.push(treePos);
+        Tree(game, {
+            position:{x: treePos[0], y:0, z:treePos[1]},
+            height: treePos[2],
+            bark: 3,
+            leaves: 4,
+            treeType: 'subspace'
+        });
     }
 };
